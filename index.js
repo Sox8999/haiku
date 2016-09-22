6e47258e043452a032587c8e25efadce54f399c3
Bryse <bwroeben@gmail.com>
2016-09-21 22:53:53 -0400

    Haiku

--- index.js
+++ index.js
@@ -1,4 +1,66 @@
+var fs = require('fs');
+
 var generateHaiku = function(data){
-	return "";
+	generateWordMap();
+	var haikuString = "";
+		if (data.length > 5) {
+		haikuString += generateHaikuLine(data, 5) + "\n"	
+		haikuString += generateHaikuLine(data, 7) + "\n"
+		haikuString += generateHaikuLine(data, 5)
+		}	
+	console.log(haikuString);
+	return haikuString;
 };
+
+var wordMap = {};
+
+function generateWordMap() {
+	if (wordMap.size > 0){
+		return;
+	}
+	try {  
+	    var data = fs.readFileSync('cmudict.dict.txt', 'utf8');
+    	var lines = data.split("\n");
+	    for (var i in lines) {								
+	    	var line = lines[i].split(" ");
+	    	var word = line[0];
+	    	var vowelCount = 0;
+		    for (var k in line) {
+		    	var piece = line[k];
+		    	if ('AEIOUY'.indexOf(piece[0]) >=0 ){
+		    		vowelCount ++
+		    	}
+		    	
+		    } 
+		    wordMap[word] = vowelCount;
+
+	    }
+
+	} catch(e) {
+	    console.log('Error:', e.stack);
+	}
+}
+
+var syllableCount = function(word) {
+
+	generateWordMap();
+	return wordMap[word];
+}
+
+var generateHaikuLine = function(data, syllables) {
+	var line = "";
+	while (syllables > 0) {
+		var random = data[Math.floor(Math.random() * 100000000 % data.length)];
+		if (syllableCount (random) <= syllables) {
+			line += random + " ";
+			syllables -= syllableCount (random);
+		}	
+	}	
+	return line.trim();
+
+}
+
+
 module.exports.generateHaiku = generateHaiku;
+module.exports.syllableCount = syllableCount;
+module.exports.generateHaikuLine = generateHaikuLine;
--- test/test.js
+++ test/test.js
@@ -1,9 +1,21 @@
-var haiku = require("./")
+var haiku = require("../index")
 
 var assert = require('assert');
 describe('generateHaiku', function() {
     it('should return an empty string if there is not enough data', function() {
-     console.log(haiku);
       assert.equal("", haiku.generateHaiku([]));
     });
+    it('should return a 3 line haiku if there are enough words passed in', function() {
+       assert.equal(3, haiku.generateHaiku("here is some data to make a haiku from and stuff".split(" ")).split("\n").length);
+     });
   });
+describe('syllableCount', function() {
+	it('should return two for baseball', function(){
+		assert.equal(2, haiku.syllableCount("baseball"));
+	});
+});
+describe('generateHaikuLine', function() {
+	it('should return two for baseball', function(){
+		assert.equal("ball ball", haiku.generateHaikuLine(["ball"], 2));
+	});
+});
